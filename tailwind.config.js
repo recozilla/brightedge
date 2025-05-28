09db9d3d20738690f80a807b21d82daf9154c538
recozilla <89862579+recozilla@users.noreply.github.com>
2025-05-28 14:57:31 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,175 @@
+import { heroui } from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [
+    heroui({
+      layout: {
+        dividerWeight: "1px", 
+        disabledOpacity: 0.45, 
+        fontSize: {
+          tiny: "0.75rem",   // 12px
+          small: "0.875rem", // 14px
+          medium: "0.9375rem", // 15px
+          large: "1.125rem", // 18px
+        },
+        lineHeight: {
+          tiny: "1rem", 
+          small: "1.25rem", 
+          medium: "1.5rem", 
+          large: "1.75rem", 
+        },
+        radius: {
+          small: "6px", 
+          medium: "8px", 
+          large: "12px", 
+        },
+        borderWidth: {
+          small: "1px", 
+          medium: "1px", 
+          large: "2px", 
+        },
+      },
+      themes: {
+        light: {
+          colors: {
+            "background": {
+              "DEFAULT": "#FFFFFF"
+            },
+            "content1": {
+              "DEFAULT": "#FFFFFF",
+              "foreground": "#11181C"
+            },
+            "content2": {
+              "DEFAULT": "#f4f4f5",
+              "foreground": "#27272a"
+            },
+            "content3": {
+              "DEFAULT": "#e4e4e7",
+              "foreground": "#3f3f46"
+            },
+            "content4": {
+              "DEFAULT": "#d4d4d8",
+              "foreground": "#52525b"
+            },
+            "divider": {
+              "DEFAULT": "rgba(17, 17, 17, 0.15)"
+            },
+            "focus": {
+              "DEFAULT": "#006FEE"
+            },
+            "foreground": {
+              "50": "#fafafa",
+              "100": "#f4f4f5",
+              "200": "#e4e4e7",
+              "300": "#d4d4d8",
+              "400": "#a1a1aa",
+              "500": "#71717a",
+              "600": "#52525b",
+              "700": "#3f3f46",
+              "800": "#27272a",
+              "900": "#18181b",
+              "DEFAULT": "#11181C"
+            },
+            "overlay": {
+              "DEFAULT": "#000000"
+            },
+            "danger": {
+              "50": "#fee7ef",
+              "100": "#fdd0df",
+              "200": "#faa0bf",
+              "300": "#f871a0",
+              "400": "#f54180",
+              "500": "#f31260",
+              "600": "#c20e4d",
+              "700": "#920b3a",
+              "800": "#610726",
+              "900": "#310413",
+              "DEFAULT": "#E11900",
+              "foreground": "#ffffff"
+            },
+            "default": {
+              "50": "#fafafa",
+              "100": "#f4f4f5",
+              "200": "#e4e4e7",
+              "300": "#d4d4d8",
+              "400": "#a1a1aa",
+              "500": "#71717a",
+              "600": "#52525b",
+              "700": "#3f3f46",
+              "800": "#27272a",
+              "900": "#18181b",
+              "DEFAULT": "#d4d4d8",
+              "foreground": "#000"
+            },
+            "primary": {
+              "50": "#e6f1fe",
+              "100": "#cce3fd",
+              "200": "#99c7fb",
+              "300": "#66aaf9",
+              "400": "#338ef7",
+              "500": "#006BFF",
+              "600": "#005bc4",
+              "700": "#004493",
+              "800": "#002e62",
+              "900": "#001731",
+              "DEFAULT": "#006BFF",
+              "foreground": "#fff"
+            },
+            "secondary": {
+              "50": "#f2eafa",
+              "100": "#e4d4f4",
+              "200": "#c9a9e9",
+              "300": "#ae7ede",
+              "400": "#9353d3",
+              "500": "#7828c8",
+              "600": "#6020a0",
+              "700": "#481878",
+              "800": "#301050",
+              "900": "#180828",
+              "DEFAULT": "#7828c8",
+              "foreground": "#fff"
+            },
+            "success": {
+              "50": "#e8faf0",
+              "100": "#d1f4e0",
+              "200": "#a2e9c1",
+              "300": "#74dfa2",
+              "400": "#45d483",
+              "500": "#11825B",
+              "600": "#12a150",
+              "700": "#0e793c",
+              "800": "#095028",
+              "900": "#052814",
+              "DEFAULT": "#11825B",
+              "foreground": "#fff"
+            },
+            "warning": {
+              "50": "#fefce8",
+              "100": "#fdedd3",
+              "200": "#fbdba7",
+              "300": "#f9c97c",
+              "400": "#f7b750",
+              "500": "#f5a524",
+              "600": "#c4841d",
+              "700": "#936316",
+              "800": "#62420e",
+              "900": "#312107",
+              "DEFAULT": "#f5a524",
+              "foreground": "#000"
+            }
+          }
+        }
+      }
+    })
+  ]
+}
